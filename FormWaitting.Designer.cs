06e42f784f8000607a74d7d7875bc606860bc3d8
Volant Lee <volant@163.com>
2021-04-20 15:10:30 +0800

    2021-04-20

--- FormWaitting.Designer.cs
+++ FormWaitting.Designer.cs
@@ -53,7 +53,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(198, 21);
             this.label1.TabIndex = 1;
-            this.label1.Text = "系统努力加载中，请稍等...";
+            this.label1.Text = "系统发现新版本，正在执行升级任务，请稍等...";
             // 
             // FormWaitting
             // 
